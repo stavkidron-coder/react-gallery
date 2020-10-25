e39f7c01f4a8c1c26719decdb885f95732bdcb53
Stav Kidron <kidronstav@gmail.com>
2020-10-25 16:01:10 -0500

    fixed syntax error: class to className in Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ class Header extends Component {
         return(
             <header className="jumbotron">
                 <div className="container">
-                    <h1>React Image Gallery <i class="fas fa-camera-retro"></i></h1>
+                    <h1>React Image Gallery <i className="fas fa-camera-retro"></i></h1>
                     <h3>Click on a picture to read a description</h3>
                 </div>
             </header>
