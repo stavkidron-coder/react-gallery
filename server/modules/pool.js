3f386a768c62c0f6f35c297da37580f829fa0254
Stav Kidron <kidronstav@gmail.com>
2020-12-06 17:46:40 -0600

    added Heroku to pool

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,25 +1,41 @@
-// Node Module that will connect to postgesql
+/**
+* You'll need to use environment variables in order to deploy your
+* pg-pool configuration to Heroku.
+* It will look something like this:
+**/
+
 const pg = require('pg');
+const url = require('url');
+let config = {};
 
-// Setup PG to connect to the database
-const Pool = pg.Pool;
+if (process.env.DATABASE_URL) {
+    // Heroku gives a url, not a connection object
+    // https://github.com/brianc/node-pg-pool
+    let params = url.parse(process.env.DATABASE_URL);
+    let auth = params.auth.split(':');
 
-const pool = new Pool({
-    database: 'react_gallery', // database name (this will change)
-    host: 'localhost', // where to find the database
-    port: 5432,        // port for finding the database
-    max: 10,           // max number of connections for the pool
-    idleTimeoutMillis: 30000 // 30 seconds before timeout/cancel query
-});
+    config = {
+        user: auth[0],
+        password: auth[1],
+        host: params.hostname,
+        port: params.port,
+        database: params.pathname.split('/')[1],
+        ssl: { rejectUnauthorized: false },
+        max: 10, // max number of clients in the pool
+        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    };
 
-// Listener setup on the pool isn't required, 
-// but can be super handy for troubleshooting.
-pool.on('connect', () => {
-    console.log('Connected to the database');
-});
+} else {
+    // only change the things on the right side of the ||
+    config = {
+        user: process.env.PG_USER || null, //env var: PGUSER
+        password: process.env.DATABASE_SECRET || null, //env var: PGPASSWORD
+        host: process.env.DATABASE_SERVER || 'localhost', // Server hosting the postgres database
+        port: process.env.DATABASE_PORT || 5432, //env var: PGPORT
+        database: process.env.DATABASE_NAME || 'react_gallery', //env var: PGDATABASE or the name of your database (e.g. database: process.env.DATABASE_NAME || 'koala_holla',)
+        max: 10, // max number of clients in the pool
+        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    };
+}
 
-pool.on('error', (error) => {
-    console.log('Error with database pool', error);
-});
-
-module.exports = pool;
+module.exports = new pg.Pool(config);
